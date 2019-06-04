8b024258103054823a5fe005df969e499df2f477
Austin Rhodes <austin.dev@outlook.com>
2019-06-04 19:32:35 -0400

    making envs available outsite of parent process

--- install-factorio-server.sh
+++ install-factorio-server.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # set env vars
+echo 'Setting ENV vars'
 curl -s http://169.254.169.254/latest/user-data > /tmp/user-data.txt
-AWS_USER_NAME="$(cut -s -f 1 -d ',' user-data.txt)"
-AWS_ACCESS_KEY="$(cut -s -f 2 -d ',' user-data.txt)"
-AWS_SECRET_KEY="$(cut -s -f 3 -d ',' user-data.txt)"
+export AWS_USER_NAME="$(cut -s -f 1 -d ',' user-data.txt)"
+export AWS_ACCESS_KEY="$(cut -s -f 2 -d ',' user-data.txt)"
+export AWS_SECRET_KEY="$(cut -s -f 3 -d ',' user-data.txt)"
 
 # is docker installed ?
 if ! [ -x "$(command -v docker)" ]; then
