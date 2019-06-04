dbf81a9150a0d3c79de43957467a63c47b644bab
Austin Rhodes <austin.dev@outlook.com>
2019-06-04 12:16:00 -0400

    deploying to /tmp/server

--- install-factorio-server.sh
+++ install-factorio-server.sh
@@ -18,7 +18,7 @@ else
 fi
 
 # verify docker compose yml exists
-if ! [ -f /tmp/docker-compose.yml ]; then
+if ! [ -f /tmp/server/docker-compose.yml ]; then
 	echo 'Error: docker-compose.yml file was not copied, try re-running deploy in circleci'
 	exit 1
 else
@@ -26,9 +26,9 @@ else
 fi
 
 # shutdown existing factorio server
-docker-compose -f /tmp/docker-compose.yml down
+docker-compose -f /tmp/server/docker-compose.yml down
 /usr/bin/printf "\xE2\x9C\x94 docker-compose down\n"
 
 # start server
-docker-compose -f /tmp/docker-compose.yml up --detach
+docker-compose -f /tmp/server/docker-compose.yml up --detach
 /usr/bin/printf "\xE2\x9C\x94 docker-compose up\n"
