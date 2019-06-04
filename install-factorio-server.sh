2d638582d0626ed4d5790d49f14be33e7c7d9741
Austin Rhodes <austin.dev@outlook.com>
2019-06-04 10:22:10 -0400

    added docker install script

--- install-factorio-server.sh
+++ install-factorio-server.sh
@@ -1,11 +1,20 @@
+#!/bin/sh
 
 # is docker installed?
-echo "hello world!"
+if ! [ -x "$(command -v docker)" ]; then
+	echo 'Warn: Docker is not installed'
 
-# maybe install docker and docker-compose
+	sudo yum install -y docker docker-compose
+fi
 
-# docker-compose down
+# shutdown existing factorio server
+docker-compose down
 
-# copy docker compose 
+# verify docker compose yml exists
+if ! [ -f /tmp/docker-compose-yml ]; then
+	echo 'Warn: docker-compose.yml file was not copied, try re-running deploy in circleci'
+	exit 1
+fi
 
-# run docker-compose up
+# start server
+docker-compose up
